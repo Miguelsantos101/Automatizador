90ab969cf1021f3a6c7e200593496326c28eab99
miguel.flores <miiguelsantos05@hotmail.com>
2023-04-12 17:20:02 -0400

    Espaços

--- Teste.py
+++ Teste.py
@@ -11,12 +11,14 @@ if teste == '1':
     passwordAVA = 'miguelsantosMM26*'
     
     ava = Ava(urlAVA, usernameAVA, passwordAVA)
+    
 elif teste == '2':
     urlManager = 'http://nccgsrv07/manager/login'
     usernameManager = '08234775170'
     passwordManager = '1111'
     
     manager = Manager(urlManager, usernameManager, passwordManager)
+    
 elif teste == '3':
     urlOnline = 'http://nccgsrv07/online/Login/Login.aspx'
     usernameOnline = '08234775170'
@@ -28,3 +30,4 @@ elif teste == '4':
     urlChangeset = 'http://nccgtfs01:8080/tfs/NotaControl/ISS%20ONLINE/_backlogs/TaskBoard/Online%20ISS%20Vers%C3%A3o%20-%20Abril%202023/Licita%C3%A7%C3%A3o%20Contagem%201.3?_a=requirements'
     
     changesets = Changesets(urlChangeset)
+
